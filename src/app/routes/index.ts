bc6c841d5565c253a782a7f68f7904eaacf0600f
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-26 09:46:52 +0600

    routes added

---
+++ src/app/routes/index.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+// import { AuthRoutes } from './modules/auth/auth.route';
+// import { ProductRoutes } from './modules/product/product.route';
+// import { OrderRoutes } from './modules/order/order.route';
+import { UserRoutes } from '../modules/User/user.route';
+
+const router = Router();
+
+const moduleRoutes = [
+//   {
+//     path: '/auth',
+//     route: AuthRoutes, // User registration, login
+//   },
+//   {
+//     path: '/products',
+//     route: ProductRoutes, // Product CRUD operations
+//   },
+//   {
+//     path: '/orders',
+//     route: OrderRoutes, // Order management and checkout
+//   },
+  {
+    path: '/users',
+    route: UserRoutes, // Manage customers and admins
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
